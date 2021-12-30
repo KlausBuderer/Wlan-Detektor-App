bb97926455d5e1633aa567ad033837ec8dcbd941
Bruno Thurnherr <bruno.thurnherr@it-wissen.ch>
2021-12-30 14:48:47 +0100

    Allg. Konfiguration

--- app/src/main/java/com/gruppe4/wlan_detektor/ui/Utility/database/DatabaseHandler.kt
+++ app/src/main/java/com/gruppe4/wlan_detektor/ui/Utility/database/DatabaseHandler.kt
@@ -8,25 +8,28 @@ class DatabaseHandler(context: Context) :
         SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION){
 
             companion object {
+
+                // Datenbank
                 private const val DATABASE_VERSION = 1
-                private const val DATABASE_NAME = "WlanDetektor"
+                private const val DATABASE_NAME = "DbWlanDetektor"
 
-            // Tabelle xx
+                // Tabellen
+                private const val TBL_MESSUNG = "tblMessung"
+                private const val TBL_MESSPUNKT = "tblMesspunkt"
+                private const val TBL_MESSPUNKTRELATION = "tblMesspunktRelation"
+                private const val TBL_RAEUMLICHKEIT = "tblMesspunkt"
+                private const val TBL_STOCKWERK = "tblStockwerk"
+                private const val TBL_MASSEINHEIT = "tblMassEinheit"
 
-            // Tabelle xx
-
-            // Tabelle xx
-
-            // Tabelle xx
-
-            // Tabelle xx
-
-            // Tabelle xx
+                // Tabelleninhalt zu Tabelle tblMesspunkt
+                private const val ID_MESSUNG = "ID"
 
             }
 
     override fun onCreate(db: SQLiteDatabase?) {
-        TODO("Not yet implemented")
+        val CREATE_TBL_MESSUNG = ("CREATE TABLE " + TBL_MESSPUNKT + "("
+                + ID_MESSUNG + "INTEGER PRIMARY KEY)")
+
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
