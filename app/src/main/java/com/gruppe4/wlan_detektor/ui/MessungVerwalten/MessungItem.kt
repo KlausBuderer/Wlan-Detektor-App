91f0fda57a2861c0ef1346c9e3b32a420991d85f
KlausBuderer <buderer.tk @gmail.com>
2021-12-31 14:13:34 +0100

    Messungsliste und Messpunkterfassungsmaske implementiert

---
+++ app/src/main/java/com/gruppe4/wlan_detektor/ui/MessungVerwalten/MessungItem.kt
@@ -0,0 +1,8 @@
+package com.gruppe4.wlan_detektor.ui.MessungVerwalten
+
+data class MessungItem(val id: Int
+                        ,val name: String
+                        ,val ssid: String
+                        ,val raeumlichkeit: String
+                        ,val datum: String
+                        ,val zeit: String)
