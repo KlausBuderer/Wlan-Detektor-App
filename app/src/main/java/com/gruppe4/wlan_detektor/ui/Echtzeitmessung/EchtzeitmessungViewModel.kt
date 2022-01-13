83396135abaabc184ba4dded3ec7e0ed3758ecde
KlausBuderer <buderer.tk @gmail.com>
2022-01-13 22:01:59 +0100

    Echtzeitmessung - SinusGenerator von Thread auf Coroutine umprogrammiert - Update nicht wieder einschaltbar bug gefixt - Ausschaltung der Coroutinen bei Bildwechsel implementiert

--- app/src/main/java/com/gruppe4/wlan_detektor/ui/Echtzeitmessung/EchtzeitmessungViewModel.kt
+++ app/src/main/java/com/gruppe4/wlan_detektor/ui/Echtzeitmessung/EchtzeitmessungViewModel.kt
@@ -6,102 +6,163 @@ import android.graphics.Color
 import android.net.ConnectivityManager
 import android.net.wifi.WifiInfo
 import android.net.wifi.WifiManager
-import android.util.Log
 import androidx.lifecycle.*
+import com.gruppe4.wlan_detektor.model.Netzwerk.NetzwerkInfo
 import com.gruppe4.wlan_detektor.model.SinusGenerator
-import com.gruppe4.wlan_detektor.ui.MessungVerwalten.MesspunktItem
 import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers.IO
 import java.lang.NullPointerException
 
 class EchtzeitmessungViewModel(application: Application) : AndroidViewModel(application) {
 
-    private lateinit var netzwerkArrayList : ArrayList<MesspunktItem>
 
-    var wifiManager = getApplication<Application>().getSystemService(Context.WIFI_SERVICE) as WifiManager
-    val connectivityManager = getApplication<Application>().getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    var wifiManager =
+        getApplication<Application>().getSystemService(Context.WIFI_SERVICE) as WifiManager
+    val connectivityManager =
+        getApplication<Application>().getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
     private var connectionInfo: WifiInfo = wifiManager.connectionInfo
-    var progressFarbeZyklisch: Int = progressBarFarbeEinstellen()
+    private var wifiKlasse: NetzwerkInfo
+    private var wifiInfos: WifiInfo
 
+    init {
+        wifiKlasse = NetzwerkInfo(application)
+        wifiInfos = wifiKlasse.getConnectionInfo()
 
-    val _netzwerkInfo = MutableLiveData<WifiInfo>().apply {
-        value = connectionInfo
     }
 
-    val _band = MutableLiveData<Double>().apply {
+    private var progressFarbeZyklisch: Int = progressBarFarbeEinstellen()
+    private val _netzwerkInfo = MutableLiveData<WifiInfo>().apply {
+        value = wifiInfos
+    }
+
+    private val _band = MutableLiveData<Double>().apply {
         value = bandUmrechnung()
     }
 
-    val _progressFarbe = MutableLiveData<Int>().apply {
+    private val _progressFarbe = MutableLiveData<Int>().apply {
         value = progressFarbeZyklisch
     }
 
-    fun progressBarFarbeEinstellen(): Int{
-        if (connectionInfo.rssi > -60){
+    private fun progressBarFarbeEinstellen(): Int {
+        if (wifiInfos.rssi > -60) {
             return Color.GREEN
-        }else if (connectionInfo.rssi > -70){
+        } else if (wifiInfos.rssi > -70) {
             return Color.YELLOW
-        }else{
+        } else {
             return Color.RED
         }
     }
 
-    fun bandUmrechnung(): Double{
-        if (connectionInfo.frequency > 5000){
+    private fun bandUmrechnung(): Double {
+        if (wifiInfos.frequency > 5000) {
             return 5.0
-        }else if (connectionInfo.frequency in 2000 .. 3000){
+        } else if (wifiInfos.frequency in 2000..3000) {
             return 2.4
         }
         return 0.0
     }
 
-    val netzwerkInfo: LiveData<WifiInfo> =  _netzwerkInfo
-    val progressFarbe: LiveData<Int> =  _progressFarbe
+    val netzwerkInfo: LiveData<WifiInfo> = _netzwerkInfo
+    val progressFarbe: LiveData<Int> = _progressFarbe
     val band: LiveData<Double> = _band
     var tonEin: Boolean = false
-    val sinusGenerator = SinusGenerator(application)
-    var routine = viewModelScope
+    private val sinusGenerator = SinusGenerator(application)
+    private lateinit var updateJob: Job
+    private lateinit var sinusJob: Job
     var frequenz: Int = 0
 
-    fun startCoroutine() {
-        routine.launch {
+    //Start Coroutine für die Abfrage der Netzwerkinformationen
+    fun startUpdateCoroutine() {
+        updateJobInit()
+        val scope = CoroutineScope(IO + updateJob).launch {
             startUpdates()
         }
-
     }
 
-    fun startSinus(){
-        netzwerkInfo.value?.let { sinusGenerator.start(it.rssi) }
-    }
-
-    fun stopSinus(){
-        sinusGenerator.stopPlaying()
-    }
-
-    fun stopCoroutine(){
-        routine.cancel()
-    }
-
-    suspend fun startUpdates(){
-      withContext(Dispatchers.IO){
-            while (isActive) {
-                Log.e("couritne: ","$isActive")
-            try {
-                connectionInfo = wifiManager.connectionInfo
-
-                _netzwerkInfo.postValue(connectionInfo)
-                _progressFarbe.postValue(run { progressBarFarbeEinstellen() })
-
-
-            }catch (e:NullPointerException){
-
+    //Stoppe Coroutine für die Abfrage der Netzwerkinformationen
+    fun stopUpdateCoroutine() {
+        if (::updateJob.isInitialized) {
+            if (updateJob.isActive || updateJob.isCompleted) {
+                updateJob.cancel("job beendet!!")
             }
-
-                delay(500)
-            }
-
+        } else {
+            updateJobInit()
         }
+    }
+
+    suspend fun startUpdates() {
+        while (updateJob.isActive) {
+            try {
+                _netzwerkInfo.postValue(wifiKlasse.getConnectionInfo())
+                _progressFarbe.postValue(run { wifiKlasse.progressBarFarbeEinstellen(wifiInfos.rssi) })
+                println(wifiKlasse.getConnectionInfo().rssi)
+            } catch (e: NullPointerException) {
+
+                println("Update nicht erfolgreich")
+            }
+            delay(500)
+        }
+    }
+
+    fun updateJobInit() {
+        updateJob = Job()
+        println("Neuer Job initialisiert")
+
+        updateJob.invokeOnCompletion {
+            it?.message.let {
+                var msg = it
+                if (msg.isNullOrBlank()) {
+                    msg = "Unbekannter Fehler aufgetreten"
+                }
+                println("$updateJob ist gecancelt. Grund: $msg")
+            }
+        }
+    }
+
+    fun sinusJobInit() {
+        sinusJob = Job()
+        println("Neuer Job initialisiert")
+
+        sinusJob.invokeOnCompletion {
+            it?.message.let {
+                var msg = it
+                if (msg.isNullOrBlank()) {
+                    msg = "Unbekannter Fehler aufgetreten"
+                }
+                println("$sinusJob ist gecancelt. Grund: $msg")
+            }
+        }
+    }
+
+    fun startSinus() {
+        sinusJobInit()
+        CoroutineScope(IO + sinusJob).launch{
+            netzwerkInfo.value?.let { sinusGenerator.start(it.rssi) }
+        }
+    }
+
+    fun stopSinus() {
+        sinusGenerator.stopPlaying()
+        if (::sinusJob.isInitialized) {
+            if (sinusJob.isActive || updateJob.isCompleted) {
+                sinusJob.cancel("job beendet!!")
+            }
+        } else {
+            sinusJobInit()
+        }
+    }
+
+    fun getSinusJobStatus():Boolean{
+        //Wenn der Job initialisiert wurde wird ein true zurückgegeben
+        return ::sinusJob.isInitialized
 
     }
 
+    fun getUpdateJobStatus():Boolean{
+        //Wenn der Job initialisiert wurde wird ein true zurückgegeben
+        return ::updateJob.isInitialized
+
+    }
 
 }
+
